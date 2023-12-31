8855972231a711e77ba75940d4485ed6eff44207
jannatdad123 <155289462+jannatdad123@users.noreply.github.com>
2023-12-31 14:01:27 +0500

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,90 @@
+# alien_0 = {'color': 'green', 'points': 5}
+# print(alien_0['color'])
+# print(alien_0['points'])
+
+
+# alien_0 = {'color': 'green', 'points': 5}
+# new_points = alien_0['points']
+# print("You just earned " + str(new_points) + " points!")
+
+# alien_0 = {'color': 'green', 'points': 5}
+# print(alien_0)
+# alien_0['x_position'] = 0
+# alien_0['y_position'] = 25
+# print(alien_0)
+
+
+
+# mydic = {}
+# mydic["name"] = 'Afroz'
+# mydic["age"] = 21
+# mydic["department"] = 'BSCS'
+# print(mydic)
+
+
+# alien_0 = {'color': 'green'}
+# print("The alien is " + alien_0['color'] + ".")
+# alien_0['color'] = 'yellow'
+# print("The alien is now " + alien_0['color'] + ".")
+
+
+
+# alien_0 = {'x_position': 0, 'y_position': 25, 'speed': 'medium'}
+# print("Original x-position: " + str(alien_0['x_position']))
+# # Move the alien to the right.
+# # Determine how far to move the alien based on its current speed.
+# if alien_0['speed'] == 'slow':
+#  x_increment = 1
+# elif alien_0['speed'] == 'medium':
+#  x_increment = 2
+# else:
+#  # This must be a fast alien.
+#  x_increment = 3
+# # The new position is the old position plus the increment.
+# alien_0['x_position'] = alien_0['x_position'] + x_increment
+# print("New x-position: " + str(alien_0['x_position']))
+
+
+
+# favorite_languages = {'jen': 'python',
+#  'sarah': 'c',
+#  'edward': 'ruby',
+#  'phil': 'python',
+#  }
+# print("Sarah's favorite language is " +
+#       favorite_languages['sarah'].title() +".")
+
+
+# favbook = {'afiii':'peer e kamil',
+#            'fajar':'faults in our stars',
+#            'emena':'abe hayat'}
+# print(favbook)
+# print("fajar's favrt book is  "   + favbook['fajar'].title()+".")
+
+
+#  Person:e a dictionary to store information about a person you know.
+# Store their first name, last name, age, and the city in which they live. You 
+# should have keys  Ussuch as first_name, last_name, age, and city. Print each 
+# piece of information stored in your dictionary
+
+
+# info = {}
+# info['first name '] = 'afroz'
+# info['last name '] = 'lal muhmd'
+# info['age'] = 21
+# info['city'] = 'turbat'
+# print(info)
+
+
+#  Use a dictionary to store people’s favorite numbers.
+# Think of five names, and use them as keys in your dictionary. Think of a favorite 
+# number for each person, and store each as a value in your dictionary. Print 
+# each person’s name and their favorite number. For even more fun, poll a few 
+# friends and get some actual data for your program.
+
+
+# fav_num = {"afii":"3"'/n',
+#            "fajar":"5"'/n',
+#            "emena":"8"'/n',
+#            "gull":"99"}
+# print(fav_num)
